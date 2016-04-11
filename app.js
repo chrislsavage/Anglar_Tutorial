ac55bd49a66bed2390fc6f3c601386ef3515f428
Chris Savage <chrislsavage@gmail.com>
2016-04-11 15:35:47 -0400

    Add object to gems. Use directive to iterate through gem objects

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 (function() {
   var app = angular.module('store', []);
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
+  var gems = [
+    {
     name: 'Dodecahedron',
     price: 2.95,
-    description: '. . .',
+    description: 'Dode you want one?',
     canPurchase: true,
-    soldOut: true,
-  }
+    soldOut: false,
+    },
+    {
+      name: "Pentagonal Gem",
+      price: 5.95,
+      description: "Pen-talking about this gem for a long time",
+      canPurchase: true,
+      soldOut: false,
+    },
+  ]
 })();
