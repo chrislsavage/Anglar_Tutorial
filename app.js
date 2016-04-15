712046ebe21e2ba810875cdae1b05ba6a9da6417
Chris Savage <chrislsavage@gmail.com>
2016-04-15 14:07:16 -0400

    Add created on date and filter to review

--- app.js
+++ app.js
@@ -31,6 +31,7 @@
     this.review = {};
 
     this.addReview = function (product){
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
       this.review = {};
     };
