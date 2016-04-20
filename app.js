3f1cfea536a3bdcbf2a0df87abe6fe7328992923
Chris Savage <chrislsavage@gmail.com>
2016-04-20 16:21:10 -0400

    Add example of using  service to do a GET to an API

--- app.js
+++ app.js
@@ -43,3 +43,12 @@
     },
   ]
 })();
+
+// example of getting an API
+// app.controller('StoreController', ['$http', function($http){
+//   var store = this;
+//   store.products = [];
+//   $http.get('/store-products.json').success(function(data){
+//     store.products = data;
+//   });
+// }]);
